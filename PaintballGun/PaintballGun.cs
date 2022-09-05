eed3bd9530fd753ccd862aae7d228369eb931baa
Owen <owen.bayliss@sky.com>
2022-09-05 22:18:30 +0100

    Add project files.

---
+++ PaintballGun/PaintballGun.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintballGun
+{
+    class PaintballGun
+    {
+        public const int MAGAZINE_SIZE = 16;
+
+        private int balls = 0;
+        private int ballsLoaded = 0;
+
+        public int GetBallsLoaded() { return ballsLoaded; }
+
+        public bool IsEmpty() { return ballsLoaded == 0;  }
+
+        public int Balls
+        {
+            get { return balls; }
+            set
+            {
+                if (value > 0)
+                    balls = value;
+                Reload();
+            }
+        }
+
+        public void Reload()
+        {
+            if (balls > MAGAZINE_SIZE)
+                ballsLoaded = MAGAZINE_SIZE;
+            else
+                ballsLoaded = balls;
+        }
+        
+        public bool Shoot()
+        {
+            if (ballsLoaded == 0) return false;
+            ballsLoaded--;
+            balls--;
+            return true;
+        }
+    }
+}
---
+++ PaintballGun/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PaintballGun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaintballGun gun = new PaintballGun();
+            while (true)
+            {
+                Console.WriteLine($"{gun.GetBalls()} balls, {gun.GetBallsLoaded()} loaded.");
+                if (gun.IsEmpty()) Console.WriteLine("WARNING: You're out of ammo");
+                Console.WriteLine("Space to shoot, r to reload, + to add ammo, q to quit");
+
+                char key = Console.ReadKey(true).KeyChar;
+                if (key == ' ') Console.WriteLine($"Shooting returned {gun.Shoot()}");
+                else if (key == 'r') gun.Reload();
+                else if (key == '+') gun.SetBalls(gun.GetBalls() + PaintballGun.MAGAZINE_SIZE);
+                else if (key == 'q') return;
+            }
+        }
+    }
+}
